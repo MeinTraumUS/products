364e0aad9c016575008bfa00c8ac7a440ee51e7c
Nikki Huang <nikkihuang309700034@gmail.com>
2022-05-25 19:19:36 +0800

    add header

--- products.py
+++ products.py
@@ -16,4 +16,11 @@ products[0][0]	# 列出清單中的第一個商品名稱
 
 for p in products:
 	print(p[0],'的價格是', p[1])
-	
+
+# "abc" +"123" = "abc123"
+# "abc" * 3 = "abcabcabc"
+
+with open('products.csv', "w", encoding="utf-8") as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + "," + p[1] + '\n')	
